73035a7e34a3baaaf26fb1ffbba4b834d4d37bbe
Suhrid-Talukder333 <suhridtalukder333@gmail.com>
2021-05-31 09:41:31 +0600

    email sender is working

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
 const nodemailer = require("nodemailer");
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
-const { json } = require("body-parser");
+const dotenv = require("dotenv");
 
-const transporter = nodemailer.createTransport({
-  service: "smtp.gmail.com",
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASS,
-  },
-});
-
+dotenv.config();
 const server = express();
 server.use(express.json());
 server.use(cors());
@@ -29,21 +18,31 @@ server.post("/", (req, res) => {
   email = person_email;
   text = person_text;
   console.log(name, email, text);
-  transporter
-    .sendMail({
-      from: '"Suhrid Talukder" <suhridtalukder333@gmail.com>',
-      to: email,
-      subject: "Thank You for your feedback",
-      text: `Hello ${name}, hope your are doing well. I am pleased to have your feedback, thank you. This is an automated email , so I will get in touch with you as soon as I can 😁`,
-    })
-    .then((info) => {
-      console.log({ info });
-      res.json({
-        check: true,
-      });
-    })
-    .catch(console.error);
+  main().catch(console.error);
+  res.json({
+    check: true,
+  });
 });
+
+const main = async () => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: '"Suhrid Talukder" <suhridtalukder333@gmail.com>',
+    to: email,
+    subject: "Thank You for your feedback",
+    text: `Hello ${name}, hope your are doing well. I am pleased to have your feedback, thank you. This is an automated email , so I will get in touch with you as soon as I can 😁`,
+  });
+  console.log("Message sent: %s", info.messageId);
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+};
 
 server.listen(3001, () => {
   console.log(`The server is running at port 3001`);
